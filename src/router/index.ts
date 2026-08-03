dd386745b3c396f3b65093b7c73b381a9098b49e
Serhii Franchuk <salsachess.gmail.com>
2026-08-04 02:47:08 +0300

    Update router for GitHub Pages deployment and set base path

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,5 @@
-import { createRouter, createWebHistory, type RouteRecordRaw } from 'vue-router'
+import { createRouter, createWebHashHistory, type RouteRecordRaw } from 'vue-router'
+...
 import PConfig from '../pages/PConfig.vue'
 import PGames from '../pages/PGames.vue'
 
@@ -29,7 +30,7 @@ const routes: RouteRecordRaw[] = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes
 })
 
